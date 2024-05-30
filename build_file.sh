2987268b2eaae3c3481f209fc9296d5206d4c3de
Rahul kumar <rahul10002000@gmail.com>
2024-05-31 01:16:10 +0530

    vercel6

--- build_file.sh
+++ build_file.sh
@@ -3,6 +3,9 @@
 # Activate virtual environment
 source /c/Users/rahul/Desktop/New/env/Scripts/activate
 
+# Update PATH to include Python and pip (if necessary)
+export PATH=/c/Users/rahul/Desktop/New/env/Scripts:$PATH
+
 # Install dependencies
 echo "Installing dependencies..."
 pip install -r requirements.txt
