074c31ba04a6213d05b2cddd8e94902857d0fd8a
Rahul kumar <rahul10002000@gmail.com>
2024-05-31 01:03:41 +0530

    vercel3

--- build_file.sh
+++ build_file.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# Use specific Python version
+PYTHON=python3.9
+
 # Install dependencies
 echo "Installing dependencies..."
-pip install -r requirements.txt
+$PYTHON -m pip install -r requirements.txt
 
 # Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
-
+$PYTHON manage.py collectstatic --noinput
